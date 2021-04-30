702936d385850f62734d71f949792c8d2492df90
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2021-04-30 13:17:18 +0300

    header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,10 +1,15 @@
-
+import styles from './Header.module.css';
 export default function Header () {
     return (
-        <div className="header">
+        <div className={styles.header}>
             <div className="logo">
                 Строитель
             </div>
+            <button className={styles.catalog__button}>
+                Каталог
+            </button>
+            <input className={styles.search} placeholder="Поиск">
+            </input>
         </div>
     )
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,13 @@
 import Header from '../components/Header';
+import styles from './index.module.css';
 
 export default function Index(){
   return (
-    <div className="page">
+    <>
+    <div className={styles.page}>
       <Header></Header>
     </div>
+    </>
   )
 
 }
